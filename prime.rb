0dcfef0658179ab7b77d7a6f101f049307fe65b4
Christian Pardo <chrispardopersonal@gmail.com>
2020-04-03 18:31:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,12 @@
-def prime?(num)
-  
-  
+def prime?(number)
+  if number < || number == 0 || number == 1
+
+    return false
+  else
+    (2..number-1).to_a.all? do |possible_factor|
+      number % possible_factor != 0
+    end
+  end
 end
+
+
